ee74c0c7815e92adc9f8eb5f5339607d25908e94
yesiah <hyenhsiang@gmail.com>
2022-11-06 17:27:45 +0800

    Lighting for sym and nav layers

--- keyboards/crkbd/keymaps/yesiah/keymap.c
+++ keyboards/crkbd/keymaps/yesiah/keymap.c
@@ -20,6 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 
+// Custom macros for shorter key code
 #define LCTLS LCTL_T(KC_S)
 #define LALTD LALT_T(KC_D)
 #define RCTLL RCTL_T(KC_L)
@@ -28,12 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define NUM_TAB LT(NUM, KC_TAB)
 #define NAV_ENT LT(NAV, KC_ENT)
 
-enum layers {
-  BASE,
-  NUM,
-  SYM,
-  NAV
-};
+enum layers { BASE, NUM, SYM, NAV };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_split_3x6_3(
@@ -75,18 +71,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [NAV] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      QK_BOOT, _______, _______, _______, _______, _______,                      KC_HOME, KC_PGUP, _______,   KC_UP, _______, _______,
+      QK_BOOT, _______, _______, _______, _______, _______,                      KC_HOME, _______,   KC_UP, _______, KC_PGUP, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, _______,                       KC_END, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______,
+      _______, _______, _______, _______, _______, _______,                       KC_END, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, _______, _______, _______, _______, _______,                      _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______,
+      _______, _______, _______, _______, _______, _______,                      _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
   )
 };
 
-// LED numbering
+// Per key, per layer RGBs
+//
+// LED indexing
 //   number: number on the build guide, 1-index
 //   id: used in code, 0-index
 // lhs id = number - 1
@@ -100,28 +98,37 @@ const rgblight_segment_t PROGMEM my_base_layer[] = RGBLIGHT_LAYER_SEGMENTS(
 const rgblight_segment_t PROGMEM my_num_layer[] = RGBLIGHT_LAYER_SEGMENTS(
     // Delete: LHS 26 white
     {25, 1, HSV_WHITE},
-    // RHS 8~10 yellow
-    {34, 3, HSV_YELLOW},
-    // RHS 11~14, 16~21 magenta
+    // Numpad: RHS 11~14, 16~21 magenta
     {37, 4, HSV_MAGENTA}, {42, 6, HSV_MAGENTA},
-    // RHS 22~24 yellow
-    {48, 3, HSV_YELLOW});
+    // Numpad symbols: RHS 8~10, 22~24 yellow
+    {34, 3, HSV_YELLOW}, {48, 3, HSV_YELLOW});
 
-// Light LEDs 11 & 12 in purple when keyboard layer 2 is active
-const rgblight_segment_t PROGMEM my_layer2_layer[] =
-    RGBLIGHT_LAYER_SEGMENTS({11, 2, HSV_PURPLE});
+const rgblight_segment_t PROGMEM my_sym_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    // Number row: LHS 10~11, 18~19, 24 Yellow
+    //             RHS 10~11, 18~19, 24 Yellow
+    {9, 2, HSV_YELLOW}, {17, 2, HSV_YELLOW}, {23, 1, HSV_YELLOW},
+    {36, 2, HSV_YELLOW}, {44, 2, HSV_YELLOW}, {50, 1, HSV_YELLOW},
+    // Backtick, minus, equal: LHS 13, 16, 21 cyan
+    {12, 1, HSV_CYAN}, {15, 1, HSV_CYAN}, {20, 1, HSV_CYAN},
+    // Symbols: LHS 9, 12, 17, 20, 23 Orange
+    //          RHS 9 12 17 Orange
+    {8, 1, HSV_ORANGE}, {11, 1, HSV_ORANGE}, {16, 1, HSV_ORANGE},
+    {19, 1, HSV_ORANGE}, {22, 1, HSV_ORANGE}, {35, 1, HSV_ORANGE},
+    {38, 1, HSV_ORANGE}, {43, 1, HSV_ORANGE},
+    // Brackets: RHS 13 16 20 21 22 23 Magenta
+    {39, 1, HSV_MAGENTA}, {42, 1, HSV_MAGENTA}, {46, 4, HSV_MAGENTA});
 
-// Light LEDs 13 & 14 in green when keyboard layer 3 is active
-const rgblight_segment_t PROGMEM my_layer3_layer[] =
-    RGBLIGHT_LAYER_SEGMENTS({13, 2, HSV_GREEN});
+const rgblight_segment_t PROGMEM my_nav_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    // Nav: RHS 9~10, 23~24 magenta
+    {35, 2, HSV_MAGENTA}, {49, 2, HSV_MAGENTA},
+    // Arrow keys: RHS 12, 17~18, 20 yellow
+    {38, 1, HSV_YELLOW}, {43, 2, HSV_YELLOW}, {46, 1, HSV_YELLOW},
+    // Volume control: RHS 13, 16, 21 cyan
+    {39, 1, HSV_CYAN}, {42, 1, HSV_CYAN}, {47, 1, HSV_CYAN});
 
 // Now define the array of layers. Later layers take precedence
-const rgblight_segment_t *const PROGMEM my_rgb_layers[] =
-    RGBLIGHT_LAYERS_LIST(my_base_layer,
-                         my_num_layer,
-                         my_layer2_layer,  // Overrides other layers
-                         my_layer3_layer   // Overrides other layers
-    );
+const rgblight_segment_t *const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    my_base_layer, my_num_layer, my_sym_layer, my_nav_layer);
 
 void keyboard_post_init_user(void) {
   // Enable the LED layers
@@ -154,87 +161,86 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 #define L_ADJUST 8
 
 void oled_render_layer_state(void) {
-    oled_write_P(PSTR("Layer: "), false);
-    switch (layer_state) {
-        case L_BASE:
-            oled_write_ln_P(PSTR("Default"), false);
-            break;
-        case L_LOWER:
-            oled_write_ln_P(PSTR("Lower"), false);
-            break;
-        case L_RAISE:
-            oled_write_ln_P(PSTR("Raise"), false);
-            break;
-        case L_ADJUST:
-        case L_ADJUST|L_LOWER:
-        case L_ADJUST|L_RAISE:
-        case L_ADJUST|L_LOWER|L_RAISE:
-            oled_write_ln_P(PSTR("Adjust"), false);
-            break;
-    }
+  oled_write_P(PSTR("Layer: "), false);
+  switch (layer_state) {
+    case L_BASE:
+      oled_write_ln_P(PSTR("Default"), false);
+      break;
+    case L_LOWER:
+      oled_write_ln_P(PSTR("Lower"), false);
+      break;
+    case L_RAISE:
+      oled_write_ln_P(PSTR("Raise"), false);
+      break;
+    case L_ADJUST:
+    case L_ADJUST | L_LOWER:
+    case L_ADJUST | L_RAISE:
+    case L_ADJUST | L_LOWER | L_RAISE:
+      oled_write_ln_P(PSTR("Adjust"), false);
+      break;
+  }
 }
-
 
 char keylog_str[24] = {};
 
 const char code_to_name[60] = {
-    ' ', ' ', ' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f',
-    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-    'R', 'E', 'B', 'T', '_', '-', '=', '[', ']', '\\',
-    '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
+    ' ', ' ',  ' ', ' ', 'a',  'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    'i', 'j',  'k', 'l', 'm',  'n', 'o', 'p', 'q', 'r', 's', 't',
+    'u', 'v',  'w', 'x', 'y',  'z', '1', '2', '3', '4', '5', '6',
+    '7', '8',  '9', '0', 'R',  'E', 'B', 'T', '_', '-', '=', '[',
+    ']', '\\', '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
 
 void set_keylog(uint16_t keycode, keyrecord_t *record) {
   char name = ' ';
-    if ((keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) ||
-        (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) { keycode = keycode & 0xFF; }
+  if ((keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) ||
+      (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) {
+    keycode = keycode & 0xFF;
+  }
   if (keycode < 60) {
     name = code_to_name[keycode];
   }
 
   // update keylog
   snprintf(keylog_str, sizeof(keylog_str), "%dx%d, k%2d : %c",
-           record->event.key.row, record->event.key.col,
-           keycode, name);
+           record->event.key.row, record->event.key.col, keycode, name);
 }
 
-void oled_render_keylog(void) {
-    oled_write(keylog_str, false);
-}
+void oled_render_keylog(void) { oled_write(keylog_str, false); }
 
 void render_bootmagic_status(bool status) {
-    /* Show Ctrl-Gui Swap options */
-    static const char PROGMEM logo[][2][3] = {
-        {{0x97, 0x98, 0}, {0xb7, 0xb8, 0}},
-        {{0x95, 0x96, 0}, {0xb5, 0xb6, 0}},
-    };
-    if (status) {
-        oled_write_ln_P(logo[0][0], false);
-        oled_write_ln_P(logo[0][1], false);
-    } else {
-        oled_write_ln_P(logo[1][0], false);
-        oled_write_ln_P(logo[1][1], false);
-    }
+  /* Show Ctrl-Gui Swap options */
+  static const char PROGMEM logo[][2][3] = {
+      {{0x97, 0x98, 0}, {0xb7, 0xb8, 0}},
+      {{0x95, 0x96, 0}, {0xb5, 0xb6, 0}},
+  };
+  if (status) {
+    oled_write_ln_P(logo[0][0], false);
+    oled_write_ln_P(logo[0][1], false);
+  } else {
+    oled_write_ln_P(logo[1][0], false);
+    oled_write_ln_P(logo[1][1], false);
+  }
 }
 
 void oled_render_logo(void) {
-    static const char PROGMEM crkbd_logo[] = {
-        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94,
-        0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4,
-        0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4,
-        0};
-    oled_write_P(crkbd_logo, false);
+  static const char PROGMEM crkbd_logo[] = {
+      0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a,
+      0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94, 0xa0,
+      0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab,
+      0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xc0, 0xc1,
+      0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc,
+      0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4, 0};
+  oled_write_P(crkbd_logo, false);
 }
 
 bool oled_task_user(void) {
-    if (is_keyboard_master()) {
-        oled_render_layer_state();
-        oled_render_keylog();
-    } else {
-        oled_render_logo();
-    }
-    return false;
+  if (is_keyboard_master()) {
+    oled_render_layer_state();
+    oled_render_keylog();
+  } else {
+    oled_render_logo();
+  }
+  return false;
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -243,4 +249,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
   return true;
 }
-#endif // OLED_ENABLE
+#endif  // OLED_ENABLE
