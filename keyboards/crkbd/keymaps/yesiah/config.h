4431452614d2e31793b4081b56ec72c065ab9d08
yesiah <hyenhsiang@gmail.com>
2022-11-08 23:36:31 +0800

    Enable handedness by EEPROM

--- keyboards/crkbd/keymaps/yesiah/config.h
+++ keyboards/crkbd/keymaps/yesiah/config.h
@@ -27,9 +27,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-#define MASTER_LEFT
+// #define MASTER_LEFT
 // #define MASTER_RIGHT
-// #define EE_HANDS
+#define EE_HANDS
 
 //#define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 120
